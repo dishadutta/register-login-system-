e1b731cf8fc7847c81cb50e90ae21a5cdfabe267
Disha Dutta <47849740+dishadutta@users.noreply.github.com>
2019-12-23 19:35:45 +0530

    Delete more-login.js

--- more-login.js
+++
@@ -1,7 +0,0 @@
-function sayHello() {
-    alert("You can directly go to various social networking login page sites through this here")
- }
- 
- function heyy() {
-     alert("Wow! It,s really nice to know how many social networking sites you have")
- }
